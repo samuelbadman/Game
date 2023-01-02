65337ea1197976961542dda424fb6290b19adeb7
samuelbadman <samuelbadman@outlook.com>
2023-01-02 08:49:11 +0000

    Added d3d12 debug methods

--- Game/source/renderer/d3d12Renderer.cpp
+++ Game/source/renderer/d3d12Renderer.cpp
@@ -2,10 +2,67 @@
 
 bool d3d12Renderer::init()
 {
+    if (mainDevice != nullptr)
+    {
+        shutdown();
+    }
+
+#ifdef _DEBUG
+	const bool enableDeugLayerResult = 
+		enableDebugLayer(false, D3D12_GPU_BASED_VALIDATION_FLAGS_NONE, false);
+	if (!enableDeugLayerResult)
+	{
+		return false;
+	}
+
+	const bool reportLiveObjectsResult = dxgiReportLiveObjects();
+	if (!reportLiveObjectsResult)
+	{
+		return false;
+	}
+#endif // _DEBUG
+
+
+
     return false;
 }
 
 bool d3d12Renderer::shutdown()
 {
     return false;
+}
+
+bool d3d12Renderer::enableDebugLayer(const bool enableGPUValidation,
+	const D3D12_GPU_BASED_VALIDATION_FLAGS gpuBasedValidationFlags,
+	const bool enableSynchonizedCommandQueueValidation) const
+{
+	// Enable debug layer
+	Microsoft::WRL::ComPtr<ID3D12Debug3> debugInterface;
+	if (FAILED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugInterface))))
+	{
+		return false;
+	}
+	debugInterface->EnableDebugLayer();
+
+	// Enable GPU based validation and synchronized command queue validation
+	debugInterface->SetEnableGPUBasedValidation(enableGPUValidation);
+	debugInterface->SetGPUBasedValidationFlags(gpuBasedValidationFlags);
+	debugInterface->SetEnableSynchronizedCommandQueueValidation(enableSynchonizedCommandQueueValidation);
+
+	return true;
+}
+
+bool d3d12Renderer::dxgiReportLiveObjects() const
+{
+	// Enable reporting of live objects
+	Microsoft::WRL::ComPtr<IDXGIDebug1> dxgiDebugInterface;
+	if (FAILED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiDebugInterface))))
+	{
+		return false;
+	}
+	if (FAILED(dxgiDebugInterface->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_IGNORE_INTERNAL)))
+	{
+		return false;
+	}
+	return true;
 }
--- Game/source/renderer/d3d12Renderer.h
+++ Game/source/renderer/d3d12Renderer.h
@@ -3,19 +3,27 @@
 #include "renderer.h"
 
 #include <dxgi1_6.h>
+#include <dxgidebug.h>
 #include <d3d12.h>
 #include <wrl.h>
 
 #pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "dxguid.lib")
 #pragma comment(lib, "d3d12.lib")
 
 class d3d12Renderer : public renderer
 {
 private:
-	Microsoft::WRL::ComPtr<ID3D12Device9> mainDevice;
+	Microsoft::WRL::ComPtr<ID3D12Device9> mainDevice = nullptr;
 
 public:
 	virtual rendererPlatform getPlatform() const final { return rendererPlatform::direct3d12; }
 	virtual bool init() final;
 	virtual bool shutdown() final;
+
+private:
+	bool enableDebugLayer(const bool enableGPUValidation,
+		const D3D12_GPU_BASED_VALIDATION_FLAGS gpuBasedValidationFlags,
+		const bool enableSynchonizedCommandQueueValidation) const;
+	bool dxgiReportLiveObjects() const;
 };
