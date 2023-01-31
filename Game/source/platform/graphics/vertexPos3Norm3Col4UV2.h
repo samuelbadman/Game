e862ca79c34b220036672a3bc6a98c145cc4b6df
samuelbadman <samuelbadman@outlook.com>
2023-01-31 20:12:13 +0000

    Readded 32-bit values to vertex structure

--- Game/source/platform/graphics/vertexPos3Norm3Col4UV2.h
+++ Game/source/platform/graphics/vertexPos3Norm3Col4UV2.h
@@ -1,13 +1,9 @@
 #pragma once
 
-#include "math/vector3d.h"
-#include "math/vector4d.h"
-#include "math/vector2d.h"
-
 struct sVertexPos3Norm3Col4UV2
 {
-	vector3d position = vector3d(0.0, 0.0, 0.0);
-	vector3d normal = vector3d(0.0, 0.0, 0.0);
-	vector4d color = vector4d(0.0, 0.0, 0.0, 0.0);
-	vector2d uv = vector2d(0.0, 0.0);
+	float position[3] = { 0.0f, 0.0f, 0.0f };
+	float normal[3] = { 0.0f, 0.0f, 0.0f };
+	float color[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
+	float uv[2] = { 0.0f, 0.0f };
 };
